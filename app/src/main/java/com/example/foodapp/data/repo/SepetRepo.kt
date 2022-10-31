21c0f58d1e2b8a5c8a334bbbeea2f21248af8dcc
Basri Ayaz <basriayaz86@gmail.com>
2022-10-31 18:44:39 +0300

    Sepet Sayfasi Sorunları Giderildi

--- app/src/main/java/com/example/foodapp/data/repo/SepetRepo.kt
+++ app/src/main/java/com/example/foodapp/data/repo/SepetRepo.kt
@@ -11,55 +11,105 @@ import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
-class SepetRepo(var sdao:SepetYemeklerDao) {
-    var sepetListesi : MutableLiveData<List<SepetYemekler>>
+class SepetRepo(var sdao: SepetYemeklerDao) {
+    var sepetListesi: MutableLiveData<List<SepetYemekler>>
 
     init {
         sepetListesi = MutableLiveData()
     }
 
-    fun sepetiGetir() : MutableLiveData<List<SepetYemekler>>{
-        return  sepetListesi
+    fun sepetiGetir(): MutableLiveData<List<SepetYemekler>> {
+        return sepetListesi
     }
 
 
-    fun tumSepetGetir(kullanici_adi:String){
-        sdao.tumSepetiGetir(kullanici_adi).enqueue(object: Callback<SepetYemeklerCevap> {
-            override fun onResponse(call: Call<SepetYemeklerCevap>?, response: Response<SepetYemeklerCevap>) {
+    fun tumSepetGetir(kullanici_adi: String) {
+        sdao.tumSepetiGetir(kullanici_adi).enqueue(object : Callback<SepetYemeklerCevap> {
+            override fun onResponse(
+                call: Call<SepetYemeklerCevap>?,
+                response: Response<SepetYemeklerCevap>
+            ) {
                 val liste = response.body()!!.sepetYemekler
+                val sonliste = ArrayList<SepetYemekler>()
 
-                val list = liste.distinctBy{it.yemek_adi}
-                sepetListesi.value = list
+
+                for (i in 0..liste.size - 1) {
+                    val yemek = SepetYemekler(
+                        liste[i].sepet_yemek_id,
+                        liste[i].yemek_adi,
+                        liste[i].yemek_resim_adi,
+                        liste[i].yemek_fiyat,
+                        liste[i].yemek_siparis_adet,
+                        "iskocyali"
+                    )
+                    if (liste[i].yemek_adi == "var") {
+                        continue
+                    }
+                    if (i != liste.size - 1) { //J nin değeri liste boyunu aşmasın diye eklendi
+                        for (j in i + 1..liste.size - 1) {
+
+                            if (yemek.yemek_adi == liste[j].yemek_adi) {
+                                liste[j].yemek_adi = "var"
+                                liste[i].yemek_siparis_adet += liste[j].yemek_siparis_adet
+                            }
+                        }
+                    }
+                }
+
+                for (i in 0..liste.size - 1) {
+                    if (liste[i].yemek_adi != "var") {
+                        sonliste.add(liste[i])
+                    }
+                }
+
+                sepetListesi.value = sonliste
 
             }
+
             override fun onFailure(call: Call<SepetYemeklerCevap>?, t: Throwable?) {
-                val emptyList:List<SepetYemekler> = emptyList()
+                val emptyList: List<SepetYemekler> = emptyList()
                 sepetListesi.value = emptyList
             }
         })
     }
-    fun sepetYemekSil(sepet_yemek_id:Int,kullanici_adi: String){
-        sdao.sepettekiYemekSil(sepet_yemek_id,kullanici_adi)
+
+    fun sepetYemekSil(sepet_yemek_id: Int, kullanici_adi: String) {
+        sdao.sepettekiYemekSil(sepet_yemek_id, kullanici_adi)
             .enqueue(object : Callback<CRUDCevap> {
-            override fun onResponse(call: Call<CRUDCevap>, response: Response<CRUDCevap>) {
-                tumSepetGetir(kullanici_adi)
-                Log.e("Silme Mesajı","Yemek Silindi")
-            }
-            override fun onFailure(call: Call<CRUDCevap>, t: Throwable?) {
-                Log.e("Hata Mesajı","Yemek Silinemedi")
-            }
-        })
+                override fun onResponse(call: Call<CRUDCevap>, response: Response<CRUDCevap>) {
+                    tumSepetGetir(kullanici_adi)
+                    Log.e("Silme Mesajı", "Yemek Silindi")
+                }
+
+                override fun onFailure(call: Call<CRUDCevap>, t: Throwable?) {
+                    Log.e("Hata Mesajı", "Yemek Silinemedi")
+                }
+            })
     }
-    fun sepeteEkle(yemek_adi:String, yemek_resim_adi:String, yemek_fiyat:Int, yemek_siparis_adet:Int, kullanici_adi:String){
-        sdao.sepeteKaydet(yemek_adi,yemek_resim_adi,yemek_fiyat, yemek_siparis_adet, kullanici_adi)
-            .enqueue(object:Callback<CRUDCevap>{
+
+    fun sepeteEkle(
+        yemek_adi: String,
+        yemek_resim_adi: String,
+        yemek_fiyat: Int,
+        yemek_siparis_adet: Int,
+        kullanici_adi: String
+    ) {
+        sdao.sepeteKaydet(
+            yemek_adi,
+            yemek_resim_adi,
+            yemek_fiyat,
+            yemek_siparis_adet,
+            kullanici_adi
+        )
+            .enqueue(object : Callback<CRUDCevap> {
                 override fun onResponse(call: Call<CRUDCevap>?, response: Response<CRUDCevap>) {
                     //Sepetten Silme işlemi burada yapılacak
-                    Log.e("Olay Mesaji","REsponse geldi Mesajı")
+                    Log.e("Olay Mesaji", "REsponse geldi Mesajı")
 
                 }
+
                 override fun onFailure(call: Call<CRUDCevap>?, t: Throwable?) {
-                    Log.e("Hata Mesaji","Hata Mesajı")
+                    Log.e("Hata Mesaji", "Hata Mesajı")
                 }
             })
     }
